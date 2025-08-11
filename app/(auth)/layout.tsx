b822a8e32ed4aea5c41fcff65f27d31006f2beaa
deepak <deepakkumardash313@gmail.com>
2025-08-12 03:45:22 +0530

    Add : Login Ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,22 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute left-4 top-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
+
       <div className="flex w-full flex-col gap-6 max-w-sm">{children}</div>
     </div>
   );
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,6 +6,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function LoginPage() {
@@ -18,11 +20,26 @@ export default function LoginPage() {
         </CardDescription>
       </CardHeader>
 
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button className="w-full" variant="outline">
           <GithubIcon className="size-4" />
           Sign in with Github
         </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="Please enter your email" />
+          </div>
+
+          <Button>Continue with email</Button>
+        </div>
       </CardContent>
     </Card>
   );
