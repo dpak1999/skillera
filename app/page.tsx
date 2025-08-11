a32f2571f6f8fc5b951372bfcdaeee7f3c37771c
deepak <deepakkumardash313@gmail.com>
2025-08-12 02:48:32 +0530

    add : dark/light theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { ModeToggle } from "@/components/ui/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
 }
