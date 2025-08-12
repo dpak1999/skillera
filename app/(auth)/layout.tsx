a663d743859d5f3a583638322df9d2f9af2b7a72
deepak <deepakkumardash313@gmail.com>
2025-08-12 18:13:47 +0530

    Add : logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
+import Logo from "@/public/logo.svg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
@@ -17,7 +19,38 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         Back
       </Link>
 
-      <div className="flex w-full flex-col gap-6 max-w-sm">{children}</div>
+      <div className="flex w-full flex-col gap-6 max-w-sm">
+        <Link
+          className="flex items-center gap-2 self-center font-medium"
+          href={"/"}
+        >
+          <Image src={Logo} alt="skillera" />
+          <span className="text-2xl">Skillera</span>
+        </Link>
+        {children}
+
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking Continue, you agree to our{" "}
+          <a
+            href="https://www.termsfeed.com/live/0798557c-c518-461f-a7ee-ac75f121fdf9"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-primary underline hover:cursor-pointer"
+          >
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.termsfeed.com/live/2ac6f570-0b19-4f79-a34d-df933040a76d"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-primary underline hover:cursor-pointer"
+          >
+            Privacy policy
+          </a>
+          .
+        </div>
+      </div>
     </div>
   );
 }
